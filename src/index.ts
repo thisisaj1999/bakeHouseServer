e6074165e908e65a846a4820619fe336bcdd20c3
CodersDen1 <com.github.adbhut@gmail.com>
2023-07-23 03:20:02 +0530

    added ItemController in the App object

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import  "dotenv/config";
 import "module-alias/register";
 import validateEnv from "./utils/validate";
 import App from "./app";
+import ItemController from "./resources/item/item.controller";
 
 
 
 validateEnv();
 
-const app = new App([],Number(process.env.port));
+
+
+const app = new App([new ItemController],Number(process.env.port));
 
 app.listen();
