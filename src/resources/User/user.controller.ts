bbecc0b60aebeedc90fbddc659208f1acb20b2e6
CodersDen1 <com.github.adbhut@gmail.com>
2023-07-28 05:18:12 +0530

    Added Add item to cart funstionality

--- src/resources/User/user.controller.ts
+++ src/resources/User/user.controller.ts
@@ -18,11 +18,9 @@ class UserController implements Controller {
    private initialiseRoutes(){
     
     this.router.post(`${this.path}/register`, validationMiddleware(validate.register), this.register);
-     
     this.router.post(`${this.path}/login`, validationMiddleware(validate.login) , this.login);
-
-
     this.router.get(`${this.path}`, authenticated , this.getUser)
+    this.router.post(`${this.path}/:userId/cart` , this.addItemToCart );
 
    }
 
@@ -49,7 +47,7 @@ class UserController implements Controller {
             ,res:Response
             ,next:NextFunction):Promise<Response | void> =>{
               try{
-                  const{email , password} = req.body;
+                  const{ email , password} = req.body;
 
                   const token = await this.UserService.login(email,password);
 
@@ -66,7 +64,27 @@ class UserController implements Controller {
           if(!req.user) {return next(new HttpException(404, 'User not logged in'));}
 
           res.status(200).json({user:req.user})
-        }   
+        } 
+        
+        
+      public addItemToCart = async(
+        req:Request
+        ,res:Response
+        ,next:NextFunction
+      ):Promise<void>=>{
+        try{
+          const userId =req.params.userId;
+          const itemId = req.body.itemID;
+          const quantity = req.body.quantity ||1;
+
+          const updatedUser = await this.UserService.addToCart(userId,itemId,quantity);
+
+          res.status(200).json(updatedUser);
+
+        }catch(err){
+        next(new HttpException(500,"Error adding item to Cart."))  
+        }
+      }
 
 
 }
--- src/resources/User/user.interface.ts
+++ src/resources/User/user.interface.ts
@@ -4,6 +4,12 @@ export default interface User extends Document{
     name:string,
     email:string,
     password:string,
-     
+    cart:CartItem[]
     isValidPassword(password:string) :Promise<Error | boolean>;
+}
+
+
+export interface CartItem{
+itemID :string,
+quantity: Number
 }
