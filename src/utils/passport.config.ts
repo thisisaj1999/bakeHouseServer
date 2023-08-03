a1a2b2946d185c3db6f99c0a48d50d4f92d15ae2
code-X-AJ <amit24032005@gmail.com>
2023-08-03 22:33:08 +0530

    added user of oauth strat to database

--- src/utils/passport.config.ts
+++ src/utils/passport.config.ts
@@ -1,31 +1,49 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../resources/User/user.model';
 
 const configurePassport = () => {
-  // Google OAuth Configuration
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: `${process.env.GOOGLE_CLIENT_ID}`,
-        clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-        callbackURL: '/api/users/auth/google/callback',
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        
+    // Google OAuth Configuration
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+                clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+                callbackURL: '/api/users/auth/google/callback',
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                console.log(profile);
+                
+                try {
+                    const newUser = {
+                        name: profile.displayName,
+                        email: profile.emails?.[0]?.value,
+                    };
+                    
+                    let user = await User.findOne({ email: newUser.email });
+                    if (user) {
+                        throw Error('User already exists!');
+                    } else {
+                        user = await User.create(newUser);
+                        done(null, user);
+                    }
+                } catch (err) {
+                    console.error(err);
+                    done(err);
+                }
+                return done(null, profile);
+            },
+        ),
+    );
 
-        return done(null, profile);
-      }
-    )
-  );
+    // Serialize and Deserialize User
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
 
-  // Serialize and Deserialize User
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-
-  passport.deserializeUser((user:any, done) => {
-    done(null, user);
-  });
+    passport.deserializeUser((user: any, done) => {
+        done(null, user);
+    });
 };
 
 export default configurePassport;
